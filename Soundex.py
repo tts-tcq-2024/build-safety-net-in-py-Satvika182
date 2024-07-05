c83348aab3334c0579cf4fa9910448149d093f02
Satvika182 <satvikatadikamalla@gmail.com>
2024-07-05 17:22:09 +0530

    Update Soundex.py

--- Soundex.py
+++ Soundex.py
@@ -10,24 +10,31 @@ def get_soundex_code(c):
     }
     return mapping.get(c, '0')  # Default to '0' for non-mapped characters
 
+def initialize_soundex(name):
+    if not name:
+        return "", ''
+    first_letter = name[0].upper()
+    return first_letter, get_soundex_code(first_letter)
+
+def update_soundex(soundex, prev_code, char):
+    code = get_soundex_code(char)
+    if code != '0' and code != prev_code:
+        soundex += code
+        prev_code = code
+    return prev_code, soundex
+
+def pad_soundex(soundex):
+    return (soundex + '0000')[:4]
+
+def process_name_chars(name, soundex, prev_code):
+    updated_soundex = [soundex]
+    for char in name[1:]:
+        if len(updated_soundex) >= 4:
+            break
+        prev_code, soundex = update_soundex(soundex, prev_code, char)
+        updated_soundex.append(soundex)
+    return ''.join(updated_soundex)
 
 def generate_soundex(name):
-    if not name:
-        return ""
-
-    # Start with the first letter (capitalized)
-    soundex = name[0].upper()
-    prev_code = get_soundex_code(soundex)
-
-    for char in name[1:]:
-        code = get_soundex_code(char)
-        if code != '0' and code != prev_code:
-            soundex += code
-            prev_code = code
-        if len(soundex) == 4:
-            break
-
-    # Pad with zeros if necessary
-    soundex = soundex.ljust(4, '0')
-
-    return soundex
+    soundex, prev_code = initialize_soundex(name)
+    return pad_soundex(process_name_chars(name, soundex, prev_code))
