6a1e06a38d85571cd64eb4aebf1ac5857befcec8
Satvika182 <satvikatadikamalla@gmail.com>
2024-07-05 18:52:17 +0530

    Update Soundex.test.py

--- Soundex.test.py
+++ Soundex.test.py
@@ -3,21 +3,18 @@ from Soundex import generate_soundex
 
 class TestSoundex(unittest.TestCase):
 
-    def test_empty_string(self):
-        self.assertEqual(generate_soundex(""), "")
+    # def test_empty_string(self):
+    #     self.assertEqual(generate_soundex(""), "")
 
     def test_single_character(self):
         self.assertEqual(generate_soundex("A"), "A000")
-        
-    def test_numerical_inputs(self):
-        self.assertEqual(generate_soundex("1234"), "0000")
-        self.assertEqual(generate_soundex("A1B2C3"), "A120")
-        self.assertEqual(generate_soundex("Z1Y2X3"), "Z020")
 
-    def test_mixed_cases(self):
-        self.assertEqual(generate_soundex("AbC"), "A120")
-        self.assertEqual(generate_soundex("aBc"), "A120")
-        self.assertEqual(generate_soundex("aBCdEf"), "A132")
+    def test_special_characters(self):
+        self.assertEqual(generate_soundex("A#B"), "A100")
+        self.assertEqual(generate_soundex("B@!#"), "B000")
+        
+    def test_words(self):
+        self.assertEqual(generate_soundex("Smith"), "S530")
 
     
 if __name__ == '__main__':
