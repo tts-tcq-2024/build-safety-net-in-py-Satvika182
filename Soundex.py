1d3ee1250d217efedc70982ee1f45e0efd7371db
Satvika182 <satvikatadikamalla@gmail.com>
2024-07-05 16:15:55 +0530

    Update Soundex.py

--- Soundex.py
+++ Soundex.py
@@ -15,20 +15,19 @@ def pad_soundex_code(soundex):
     return soundex.ljust(4, '0')
 
 def process_name(name, mapping):
-    def process_char(char, prev_code):
-        code = get_soundex_code(char, mapping)
-        if code != '0' and code != prev_code:
-            return code, code
-        return '', prev_code
-
     soundex = [name[0].upper()]
     prev_code = get_soundex_code(soundex[0], mapping)
-    
+    append_code = soundex.append
+    code_length = 1
+
     for char in name[1:]:
-        code, prev_code = process_char(char, prev_code)
-        if code:
-            soundex.append(code)
-        if len(soundex) >= 4:
+        code = get_soundex_code(char, mapping)
+        if code == '0' or code == prev_code:
+            continue
+        append_code(code)
+        prev_code = code
+        code_length += 1
+        if code_length == 4:
             break
 
     return ''.join(soundex)
