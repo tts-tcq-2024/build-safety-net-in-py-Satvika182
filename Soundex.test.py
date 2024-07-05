cc6512f3f1b73407ab94646e5a5c19de1482fc9a
Satvika182 <satvikatadikamalla@gmail.com>
2024-07-05 18:54:52 +0530

    Update Soundex.test.py

--- Soundex.test.py
+++ Soundex.test.py
@@ -3,11 +3,11 @@ from Soundex import generate_soundex
 
 class TestSoundex(unittest.TestCase):
 
-    # def test_empty_string(self):
-    #     self.assertEqual(generate_soundex(""), "")
+    def test_empty_string(self):
+        self.assertEqual(generate_soundex(""), "")
 
     def test_single_character(self):
-        self.assertEqual(generate_soundex("A"), "A000")
+        self.assertEqual(generate_soundex("Z"), "Z000")
 
     def test_special_characters(self):
         self.assertEqual(generate_soundex("A#B"), "A100")
