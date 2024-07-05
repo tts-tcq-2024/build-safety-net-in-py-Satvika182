5bda383f0d9c1a1049d7c6083f4c34078762b5dd
Satvika182 <satvikatadikamalla@gmail.com>
2024-07-05 15:27:32 +0530

    Update Soundex.py

--- Soundex.py
+++ Soundex.py
@@ -1,6 +1,5 @@
-def get_soundex_code(c):
-    c = c.upper()
-    mapping = {
+def get_soundex_mapping():
+    return {
         'B': '1', 'F': '1', 'P': '1', 'V': '1',
         'C': '2', 'G': '2', 'J': '2', 'K': '2', 'Q': '2', 'S': '2', 'X': '2', 'Z': '2',
         'D': '3', 'T': '3',
@@ -8,25 +7,32 @@ def get_soundex_code(c):
         'M': '5', 'N': '5',
         'R': '6'
     }
-    return mapping.get(c, '0')  # Default to '0' for non-mapped characters
 
+def get_soundex_code(c, mapping):
+    return mapping.get(c.upper(), '0')
+
+def pad_soundex_code(soundex):
+    return soundex.ljust(4, '0')
 
 def generate_soundex(name):
     if not name:
         return ""
 
-    # Start with the first letter (capitalized)
+    mapping = get_soundex_mapping()
     first_letter = name[0].upper()
     soundex = [first_letter]
-    prev_code = get_soundex_code(first_letter)
+    prev_code = get_soundex_code(first_letter, mapping)
 
-    for char in name[1:]:
-        code = get_soundex_code(char)
+    def process_char(char):
+        nonlocal prev_code
+        code = get_soundex_code(char, mapping)
         if code != '0' and code != prev_code:
             soundex.append(code)
             prev_code = code
-        if len(soundex) == 4:
+        return len(soundex) == 4
+
+    for char in name[1:]:
+        if process_char(char):
             break
 
-    # Join the list into a string and pad with zeros if necessary
-    return ''.join(soundex).ljust(4, '0')
+    return pad_soundex_code(''.join(soundex))
